56769130a16868273f98a101c662fec0a71283a4
Robert Payne <rjp5473@yahoo.com>
2020-11-01 23:33:00 -0500

    Commit app add

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,6 +5,7 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputFielddate = d3.select("#datetime");
+var columns =["datetime", "city", "state", "country", "shape", "durationMinutes", "comments" ]
 
 //Select the form
 
