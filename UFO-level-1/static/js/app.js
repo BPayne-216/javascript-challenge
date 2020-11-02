a437fc5d35c40c6f4c65c3f4332242178873c901
Robert Payne <rjp5473@yahoo.com>
2020-11-01 23:27:24 -0500

    Commit adding app code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,5 +2,10 @@
 var tableData = data;
 
 // Select the Button
-var button = d3.select("#button");
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFielddate = d3.select("#datetime");
 
+//Select the form
+
+//
