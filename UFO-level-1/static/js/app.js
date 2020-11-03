6a6d7e94ed3e3842d94fdbe2368349121565cba2
Robert Payne <rjp5473@yahoo.com>
2020-11-02 23:25:37 -0500

    Commit Level I complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,12 +1,37 @@
 // from data.js
 var tableData = data;
 
-// Select the Button
-var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputFielddate = d3.select("#datetime");
-var columns =["datetime", "city", "state", "country", "shape", "durationMinutes", "comments" ]
+// Reveal the entire table
+tableData.forEach(appendTable);
+//Select Filter table button
+var filterButton = d3.select("#filter-btn");
 
-//Select the form
+filterButton.on("click", function() {
+    //Remove any dates from list
+    d3.select("tbody").html("");
+    //Prevent page from refreshing
+    d3.event.preventDefault();
+    //Get the value property of the input element
+    var dateTime = d3.select("#datetime").property("value");
+    console.log(dateTime);
+    var filteredData = tableData.filter(record => record.datetime === dateTime);
+    console.log(filteredData)
+      
+    filteredData.forEach(appendTable); 
+});
 
-//
+function appendTable(report) {
+    var tbody = d3.select("tbody");
+
+    var row = tbody.append("tr");
+
+    Object.entries(report).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+
+        if (key === "comments") {
+            cell.attr("class", "record-comments")
+        }
+    });
+}
+
